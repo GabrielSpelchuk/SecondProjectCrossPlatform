7f7240693b3d52e68250800b2c21619869ebe672
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-04-11 15:01:34 +0300

    Project created

---
+++ src/main/java/com/example/SecondProject/SecondProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.SecondProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.List;
+
+@SpringBootApplication
+public class SecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectApplication.class, args);
+	}
+
+}
