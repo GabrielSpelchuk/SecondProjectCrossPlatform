a0e22a3b9a6189d03d822b8142f2f14e56d3c673
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-04-16 13:17:12 +0300

    Commit before adding content

--- src/main/java/com/example/SecondProject/controllers/HomeController.java
+++ src/main/java/com/example/SecondProject/controllers/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -12,7 +11,15 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Привіт зі стартової сторінки");
+        var name = "Іван";
+        var show = true;
+        var listOfFruits = List.of("Apple", "Banana", "Orange");
+        var listOfNumbers = List.of(10, 20, 30, 40);
+
+        model.addAttribute("name", name);
+        model.addAttribute("show", show);
+        model.addAttribute("items", listOfFruits);
+        model.addAttribute("numbers", listOfNumbers);
         return "home";
     }
 }
